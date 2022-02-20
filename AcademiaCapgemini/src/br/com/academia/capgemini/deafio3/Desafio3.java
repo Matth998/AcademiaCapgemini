dc190e5ed62cf8a664c0f47a4274e9de5c8a35d8
Matth998 <matheusoliveira.3125@gmail.com>
2022-02-20 14:04:12 -0300

    Upload Desafio3

--- AcademiaCapgemini/src/br/com/academia/capgemini/deafio3/Desafio3.java
+++ AcademiaCapgemini/src/br/com/academia/capgemini/deafio3/Desafio3.java
@@ -3,9 +3,20 @@ package br.com.academia.capgemini.deafio3;
 public class Desafio3 {
 
 	public static void main(String[] args) {
-		
-		
 
+		String word = Anagram.DigitTheWord();
+		char vector[];
+		
+		word = Anagram.RemoveSpace(word);
+
+		vector = new char[word.length()];
+
+		Anagram.WordBreak(word, vector);
+
+		Anagram.CalculatingAnagram(vector, 0);
+
+		System.exit(0);
 	}
 
 }
+
